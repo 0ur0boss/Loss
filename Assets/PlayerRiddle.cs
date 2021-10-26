8fdd4d46b9b3258fc8689ff9caf16fa6ea60d5f4
emmaflt <emmafouillat@gmail.com>
2021-10-26 17:16:49 +0200

    enigme cristaux faites

---
+++ Assets/PlayerRiddle.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRiddle : MonoBehaviour
+{
+    [SerializeField] List<int> solution; // 1 4 3 6        4
+    List<int> currentAnswer; // 1 4 3 5 6 7 5 1 4 3 6     11
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentAnswer = new List<int>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CheckSolution())
+        {
+            Win();
+        }
+    }
+
+    public void AddRiddleAnswer(int answer)
+    {
+        currentAnswer.Add(answer);
+    }
+
+    public bool CheckSolution()
+    {
+        if (solution.Count > currentAnswer.Count)
+        {
+            return false;
+        } else
+        {
+            bool isGood = true;
+            for (int i = 0; i < solution.Count; i++)
+            {
+                if (solution[i] != currentAnswer[currentAnswer.Count - solution.Count + i])
+                {
+                    isGood = false;
+                }
+            }
+            return isGood;
+        }
+       
+    }
+
+    public void Win()
+    {
+        Debug.Log("GAGNEEEEE");
+    }
+}
