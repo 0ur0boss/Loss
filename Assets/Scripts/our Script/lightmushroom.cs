59b17dff3b5d51521e947330b563ab0cd4cb7a1a
emmaflt <emmafouillat@gmail.com>
2021-10-27 12:32:18 +0200

    push

--- Assets/Scripts/our Script/lightmushroom.cs
+++ Assets/Scripts/our Script/lightmushroom.cs
@@ -7,23 +7,50 @@ public class lightmushroom : MonoBehaviour
 {
 
     [SerializeField] List<LampLigth> lightsToSwitch;
+    public bool isInsideTheZone = false;
+    public bool succeed = false;
 
     void Start()
     {
         foreach (LampLigth currentLight in lightsToSwitch)
         {
-            if (currentLight.getRadius() == radiusMin);
+
 
         }
 
     }
 
-    
+    void OnTriggerEnter2D(Collider2D other)
+    {
 
+        if (other.tag == "Player")
+        {
+            isInsideTheZone = true;
+            player = other.gameObject;
+        }
+    }
 
+    void OnTriggerExit2D(Collider2D other)
+    {
+
+        if (other.tag == "Player") //taguer player au préalable 
+        {
+            isInsideTheZone = false;
+        }
+    }
 
     void Update()
     {
-      
+        if (isInsideTheZone)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+
+
+            }
+            else
+            {
+            }
+        }
     }
 }
