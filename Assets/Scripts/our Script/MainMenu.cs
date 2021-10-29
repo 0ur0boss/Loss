9ed59a1134d99f16ef42985b2c5920b3766f5cc2
emmaflt <emmafouillat@gmail.com>
2021-10-29 11:19:25 +0200

    dialogues ok

--- Assets/Scripts/our Script/MainMenu.cs
+++ Assets/Scripts/our Script/MainMenu.cs
@@ -5,7 +5,10 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
+        PlayerPrefs.DeleteAll();
+        Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+       
     }
 
     public void SettingsButton()
